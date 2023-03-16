4d0b7a9dedde05d20ffe6c56e6b286d9aba9e282
Vinicius Hoffmann <vinicius_lemao.oliver@hotmail.com>
2023-03-16 15:26:44 -0300

    Criando o site

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'nome': 'Vinicius Hoffmann'})
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html', context={'home': 'Home'})
